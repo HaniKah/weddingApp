7e2ac321c3296fe331cdafd1ab7503e0a8bafc30
hani <hanikahhaleh@gmail.com>
2025-08-24 20:50:12 +0300

    implement safeAreaProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,8 @@ import {useFonts} from 'expo-font';
 import {Stack} from 'expo-router';
 import {StatusBar} from 'expo-status-bar';
 import 'react-native-reanimated';
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {StyleSheet} from "react-native";
 
 
 export default function RootLayout() {
@@ -16,14 +18,24 @@ export default function RootLayout() {
     }
 
     return (
-        <>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-                <Stack.Screen name="+not-found"/>
-            </Stack>
-            <StatusBar style="auto"/>
-        </>
+
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container} edges={['top']}>
+                <Stack>
+                    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                    <Stack.Screen name="+not-found"/>
+                </Stack>
+                <StatusBar style="auto"/>
+            </SafeAreaView>
+        </SafeAreaProvider>
 
 
     );
+
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+});

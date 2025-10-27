1407344a6ae29265517202b46cf4190958587553
hani <hanikahhaleh@gmail.com>
2025-10-27 14:20:10 +0100

    fix pick user type

--- app/onboarding/choose-role.tsx
+++ app/onboarding/choose-role.tsx
@@ -1,23 +1,23 @@
 import AppButton from "@/components/appComponents/AppButton";
 import AppView from "@/components/appComponents/AppView";
 import {StyleSheet, Text, View} from "react-native";
-import {Role} from "@/types/open-api";
 import {useAuthStore} from "@/utils/authStore";
 import {ButtonType} from "@/styles/Button";
+import {UserType} from "@/types/user-type";
 
 export default function ChooseRole() {
-    const {completeOnboarding, switchRole, role} = useAuthStore()
+    const {completeOnboarding, switchRole, userType} = useAuthStore()
 
 
     return (
         <>
             <AppView withPadding>
                 <View style={styles.container}>
-                    <Text>I am a : {role}</Text>
-                    <AppButton buttonType={ButtonType.OUTLINED} onPress={() => switchRole(Role.User)}>
+                    <Text>I am a : {userType}</Text>
+                    <AppButton buttonType={ButtonType.OUTLINED} onPress={() => switchRole(UserType.User)}>
                         Bride / Groom
                     </AppButton>
-                    <AppButton buttonType={ButtonType.OUTLINED} onPress={() => switchRole(Role.Vendor)}>
+                    <AppButton buttonType={ButtonType.OUTLINED} onPress={() => switchRole(UserType.Vendor)}>
                         Vendor
                     </AppButton>
                     <AppButton onPress={completeOnboarding}>

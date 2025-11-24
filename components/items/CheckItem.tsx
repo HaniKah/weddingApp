6526f6b06009ed78461b1a74ee8b52369af824d1
hani <hanikahhaleh@gmail.com>
2025-11-24 11:59:58 +0100

    style: padding to checklist

--- components/items/CheckItem.tsx
+++ components/items/CheckItem.tsx
@@ -71,6 +71,7 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
+        paddingHorizontal: 10
     },
 
     symbolContainer: {

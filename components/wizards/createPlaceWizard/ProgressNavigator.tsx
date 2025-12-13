f885c275d930bb480a91d55c51804137e775eea6
hani <hanikahhaleh@gmail.com>
2025-12-13 11:50:40 +0100

    styles fixes on progressNavigator

---
+++ components/wizards/createPlaceWizard/ProgressNavigator.tsx
@@ -0,0 +1,74 @@
+import {StyleSheet, View} from "react-native";
+import {Theme} from "@/styles/Theme";
+import AppButton from "@/components/appComponents/AppButton";
+import {ButtonType} from "@/styles/Button";
+
+export default function ProgressNavigator({onPrevious, onNext, isFirstStep, isLastStep}: {
+    onPrevious: () => void,
+    onNext: () => void,
+    isFirstStep?: boolean,
+    isLastStep?: boolean,
+}) {
+    return (
+        <><View style={styles.container}>
+
+
+            <View style={styles.progressContainer}>
+                <View style={styles.progressBar}></View>
+            </View>
+            <View style={styles.navigatorContainer}>
+                <View>
+                    <AppButton
+                        buttonType={ButtonType.PLAIN}
+                        onPress={onPrevious}
+                        disable={isFirstStep}>
+                        previous
+                    </AppButton>
+                </View>
+                <View>
+                    <AppButton
+                        fullRound
+                        onPress={onNext}
+                        disable={isLastStep}>
+                        {isLastStep ? "upload and Finish" : "next"}
+                    </AppButton>
+                </View>
+
+            </View>
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    }
+    ,
+    progressContainer: {
+        backgroundColor: Theme.colors.gray.S200,
+
+    },
+
+    progressBar: {
+        backgroundColor: Theme.colors.primary,
+        height: 2,
+        width: "30%",
+    },
+
+    navigatorContainer: {
+        backgroundColor: Theme.colors.white,
+        paddingTop: 20,
+        paddingBottom: 35,
+        paddingHorizontal: 35,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+
+
+})

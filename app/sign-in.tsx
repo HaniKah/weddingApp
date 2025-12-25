4171080c6d5017af8c2307e49797a3aabf6da9fc
hani <hanikahhaleh@gmail.com>
2025-12-25 20:10:52 +0100

    add test button in signIN

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -2,10 +2,24 @@ import {Button, Text} from "react-native";
 import {useAuth} from "@/contexts/auth-context";
 import AppView from "@/components/appComponents/AppView";
 import {useAuthStore} from "@/utils/authStore";
+import Constants from "expo-constants"
+import {useState} from "react";
+import {useApi} from "@/utils/api";
 
 export default function SignIn() {
     const {signInWithGoogle} = useAuth()
     const {resetOnboarding} = useAuthStore()
+    const API = useApi()
+    const [hello, setHello] = useState<string>()
+
+    async function getHello() {
+        try {
+            const resp = await API.appControllerGetHello()
+            setHello(resp.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     return (
@@ -13,10 +27,13 @@ export default function SignIn() {
             <Text>
                 Sign in
             </Text>
-            <Button onPress={signInWithGoogle} title="Sign In with google">
-            </Button>
-            <Button onPress={resetOnboarding} title="reset onboarding ">
-            </Button>
+            <Button onPress={signInWithGoogle} title="Sign In with google"/>
+            <Button onPress={resetOnboarding} title="reset onboarding "/>
+            <Text>
+                {Constants?.expoConfig?.scheme}
+            </Text>
+            <Button onPress={getHello} title="get Hello"/>
+            <Text>{hello}</Text>
         </AppView>
     )
 }

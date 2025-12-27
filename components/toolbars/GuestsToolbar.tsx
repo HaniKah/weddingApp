354964af96c46ceb38fd2e64b1780af9ac71043d
hani <hanikahhaleh@gmail.com>
2025-12-27 14:32:28 +0100

    add title to the guestlist

--- components/toolbars/GuestsToolbar.tsx
+++ components/toolbars/GuestsToolbar.tsx
@@ -1,12 +1,22 @@
 import Toolbar from "@/components/toolbars/Toolbar";
 import {IconButton} from "@/components/symbols/IconButton";
+import {StyleSheet, Text} from "react-native";
+import {Theme} from "@/styles/Theme";
 
 export default function GuestsToolbar({onCreateGuest}: { onCreateGuest: () => void }) {
     return (
         <>
             <Toolbar style={{flexDirection: "row-reverse", padding: 20}}>
                 <IconButton onPress={onCreateGuest} name="plus"/>
+                <Text style={styles.title}>Guest list</Text>
             </Toolbar>
         </>
     )
 }
+const styles = StyleSheet.create({
+    title: {
+        flex: 1,
+        fontSize: Theme.sizes.xl,
+        fontWeight: "bold",
+    }
+})

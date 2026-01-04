ce5819e17291282efe6d5248f52c24d219ee40a7
hani <hanikahhaleh@gmail.com>
2026-01-04 17:32:55 +0100

    add key extractor

--- components/wizards/plannerWizard/PickPlace.tsx
+++ components/wizards/plannerWizard/PickPlace.tsx
@@ -43,6 +43,7 @@ export function PickPlace({data, isScrollDown, setPagination}: {
                       onScrollEndDrag={onScrollEnd}
                       scrollEventThrottle={100}
                       onEndReached={onEndReached}
+                      keyExtractor={(item) => item.id.toString()}
             />
         </View>
     )

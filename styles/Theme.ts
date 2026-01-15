eabbb9bdbcb4cb96132d31cc3658c6617e28ca15
hani <hanikahhaleh@gmail.com>
2026-01-15 12:48:45 +0100

    add blue shades

--- styles/Theme.ts
+++ styles/Theme.ts
@@ -25,6 +25,17 @@ export const Theme = {
             S700: '#7B1717',
             S800: '#4b0b0b',
         },
+        blue: {
+            S100: '#D6ECF8',
+            S200: '#A3D5F0',
+            S300: '#70BEE6',
+            S400: '#439FDB',
+            S500: '#277FD0', // main “primary” / action blue
+            S600: '#1D61A0',
+            S700: '#174B7B',
+            S800: '#0B2E4B',
+        },
+
         black: '#000000',
         white: '#FFFFFF',
         gray: {

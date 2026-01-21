05467251de74835c20a97a0439f6d2a86a7e7e30
hani <hanikahhaleh@gmail.com>
2026-01-21 12:33:18 +0100

    remove unnecessary variable

--- components/appComponents/AppPressable.tsx
+++ components/appComponents/AppPressable.tsx
@@ -4,30 +4,8 @@ import Animated, {useAnimatedStyle, useSharedValue, withSpring} from "react-nati
 import type * as React from "react";
 import {forwardRef} from "react";
 
-// export default function AppPressable(props: PressableProps & React.RefAttributes<View>) {
-//
-//
-//     const scale = useSharedValue(1)
-//     const scaleAnimated = useAnimatedStyle(() => ({
-//         transform: [{scale: scale.value}]
-//     }))
-//
-//
-//     return (
-//         <Pressable {...props}
-//                    onPressIn={() => scale.value = withSpring(0.95)}
-//                    onPressOut={() => scale.value = withSpring(1)}>
-//             {(state: PressableStateCallbackType) => (
-//                 <Animated.View style={scaleAnimated}>
-//                     {typeof props.children === "function" ? props.children(state) : props.children}
-//                 </Animated.View>
-//             )}
-//         </Pressable>
-//     )
-// }
-
 const AppPressable = forwardRef<any, PressableProps>((props, ref) => {
-    const {children, onPressIn, onPressOut, ...rest} = props;
+
 
     const scale = useSharedValue(1);
     const opacity = useSharedValue(1);
@@ -39,21 +17,21 @@ const AppPressable = forwardRef<any, PressableProps>((props, ref) => {
     return (
         <Pressable
             ref={ref}
-            {...rest}
+            {...props}
             onPressIn={(e) => {
                 scale.value = withSpring(0.95);
                 opacity.value = withSpring(0.8);
-                onPressIn?.(e); // this is just if we add onPressIn prop to AppPressable
+                props.onPressIn?.(e); // this is just if we add onPressIn prop to AppPressable
             }}
             onPressOut={(e) => {
                 scale.value = withSpring(1);
                 opacity.value = withSpring(1);
-                onPressOut?.(e);
+                props.onPressOut?.(e);
             }}
         >
             {(state: PressableStateCallbackType) => (
                 <Animated.View style={scaleAnimated}>
-                    {typeof children === "function" ? children(state) : children}
+                    {typeof props.children === "function" ? props.children(state) : props.children}
                 </Animated.View>
             )}
         </Pressable>

568a4ded1856c78ed55294cd6241e039bf9492c2
hani <hanikahhaleh@gmail.com>
2026-04-18 18:10:42 +0200

    remove commented code

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -1,141 +1,119 @@
-import { AuthError, AuthRequestConfig, DiscoveryDocument, makeRedirectUri, useAuthRequest } from 'expo-auth-session';
-import React, { useEffect } from 'react';
+import {AuthError, AuthRequestConfig, DiscoveryDocument, makeRedirectUri, useAuthRequest} from 'expo-auth-session';
+import React, {useEffect} from 'react';
 import * as WebBrowser from 'expo-web-browser';
-import { useApi } from '@/utils/api';
-import { useAuthStore } from '@/utils/authStore';
-
-
-// export type AuthUser = {
-//     id: string;
-//     email: string;
-//     name: string;
-//     picture?: string;
-//     given_name?: string;
-//     family_name?: string;
-//     email_verified?: boolean;
-//     provider?: string;
-//     exp?: number;
-//     cookieExpiration?: number; // Added for web cookie expiration tracking
-// };
-// export type AuthUser = {
-//     firstName: string,
-//     lastName: string,
-//     email: string,
-// }
+import {useApi} from '@/utils/api';
+import {useAuthStore} from '@/utils/authStore';
 
 const AuthContext = React.createContext({
-  // user: null as AuthUser | null,
-  signInWithGoogle: () => {
-  },
-  signOut: () => {
-  },
-  isLoading: false,
-  error: null as AuthError | null,
+    // user: null as AuthUser | null,
+    signInWithGoogle: () => {
+    },
+    signOut: () => {
+    },
+    isLoading: false,
+    error: null as AuthError | null,
 });
 
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+export const AuthProvider = ({children}: { children: React.ReactNode }) => {
 
-  // this is just for performance
-  useEffect(() => {
-    WebBrowser.warmUpAsync();
+    // this is just for performance
+    useEffect(() => {
+        WebBrowser.warmUpAsync();
 
-    return () => {
-      WebBrowser.coolDownAsync();
+        return () => {
+            WebBrowser.coolDownAsync();
+        };
+    }, []);
+
+    const API = useApi();
+
+    const discovery: DiscoveryDocument = {
+        authorizationEndpoint: `${API.instance.getUri()}/api/auth/google/login`,
+        // tokenEndpoint: `${process.env.EXPO_PUBLIC_API_URL}/api/auth/google/token`,
+
     };
-  }, []);
-
-  const API = useApi();
-
-  const discovery: DiscoveryDocument = {
-    authorizationEndpoint: `${API.instance.getUri()}/api/auth/google/login`,
-    // tokenEndpoint: `${process.env.EXPO_PUBLIC_API_URL}/api/auth/google/token`,
-
-  };
-  const config: AuthRequestConfig = {
-    clientId: 'google',
-    redirectUri: makeRedirectUri(),
-    // scopes: ["openid", "profile", "email"], //defined in the backend
-  };
-
-  // const [user, setUser] = React.useState<AuthUser | null>(null);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [error, setError] = React.useState<AuthError | null>(null);
-  const [request, response, promptAsync] = useAuthRequest(config, discovery);
-
-  // we are not using useAuthRequest because we are implementing oAuth2.0 with passport in the backend
-  // const [request, response, promptAsync] = useAuthRequest(config, discovery)
-
-  const { logIn, logOut } = useAuthStore();
-
-
-  WebBrowser.maybeCompleteAuthSession();
-
-
-  const signInWithGoogle = async () => {
-    try {
-      if (!request) {
-        console.log('No request fon google sign in');
-        return;
-      }
-      await promptAsync();
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  // useEffect(() => {
-  //     console.log("request: ", request)
-  // }, [request]);
-
-  useEffect(() => {
-    const exchangeToken = async () => {
-      if (response?.type === 'success') {
-        await exchangeWithToken(response.params.exchangeToken);
-      }
+    const config: AuthRequestConfig = {
+        clientId: 'google',
+        redirectUri: makeRedirectUri(),
+        // scopes: ["openid", "profile", "email"], //defined in the backend
     };
-    exchangeToken();
-  }, [response]);
 
-  const signOut = async () => {
-    try {
-      await API.api.authControllerSignOut();
-      logOut();
-    } catch (error) {
-      console.error(error);
-    }
+    // const [user, setUser] = React.useState<AuthUser | null>(null);
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [error, setError] = React.useState<AuthError | null>(null);
+    const [request, response, promptAsync] = useAuthRequest(config, discovery);
 
-  };
+    // we are not using useAuthRequest because we are implementing oAuth2.0 with passport in the backend
+    // const [request, response, promptAsync] = useAuthRequest(config, discovery)
 
-  const exchangeWithToken = async (code: string) => {
-    const response = await API.api.authControllerExchangeToken({
-      headers: {
-        Authorization: `Bearer ${code}`,
-      },
-    });
-    logIn(response.data.accessToken, response.data.refreshToken, response.data.user.firstName, response.data.user.lastName, response.data.user.email);
+    const {logIn, logOut} = useAuthStore();
 
-  };
 
-  return (
-    <AuthContext.Provider value={{
+    WebBrowser.maybeCompleteAuthSession();
 
-      signInWithGoogle,
-      signOut,
-      isLoading,
-      error,
 
-    }}>
-      {children}
-    </AuthContext.Provider>
+    const signInWithGoogle = async () => {
+        try {
+            if (!request) {
+                console.log('No request fon google sign in');
+                return;
+            }
+            await promptAsync();
+        } catch (e) {
+            console.error(e);
+        }
+    };
 
-  );
+
+    useEffect(() => {
+        const exchangeToken = async () => {
+            if (response?.type === 'success') {
+                await exchangeWithToken(response.params.exchangeToken);
+            }
+        };
+        exchangeToken();
+    }, [response]);
+
+    const signOut = async () => {
+        try {
+            await API.api.authControllerSignOut();
+            logOut();
+        } catch (error) {
+            console.error(error);
+        }
+
+    };
+
+    const exchangeWithToken = async (code: string) => {
+        const response = await API.api.authControllerExchangeToken({
+            headers: {
+                Authorization: `Bearer ${code}`,
+            },
+        });
+        logIn(response.data.accessToken, response.data.refreshToken, response.data.user.firstName, response.data.user.lastName, response.data.user.email);
+
+    };
+
+    return (
+        <AuthContext.Provider value={{
+
+            signInWithGoogle,
+            signOut,
+            isLoading,
+            error,
+
+        }}>
+            {children}
+        </AuthContext.Provider>
+
+    );
 };
 
 export const useAuth = () => {
-  const context = React.useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
+    const context = React.useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
 };

e7a6adce42da1e016973933c5a0134bbeff7958c
hani <hanikahhaleh@gmail.com>
2026-05-04 10:58:27 +0200

    optionally store firstName, lastname and email

--- contexts/auth-context.tsx
+++ contexts/auth-context.tsx
@@ -1,228 +1,233 @@
-import {AuthRequestConfig, DiscoveryDocument, makeRedirectUri, useAuthRequest} from 'expo-auth-session';
-import React, {useEffect} from 'react';
+import { AuthRequestConfig, DiscoveryDocument, makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import React, { useEffect } from 'react';
 import * as WebBrowser from 'expo-web-browser';
-import {useApi} from '@/utils/api';
-import {useAuthStore} from '@/utils/authStore';
-import {SignInDto, SignUpDto} from '@/types/open-api';
+import { useApi } from '@/utils/api';
+import { useAuthStore } from '@/utils/authStore';
+import { SignInDto, SignUpDto } from '@/types/open-api';
 
 interface AuthContextType {
-    signInWithApple: () => void,
-    signInWithGoogle: () => void,
-    signInWithEmail: (data: SignInDto) => void,
-    signUpWithEmail: (data: SignUpDto) => void,
-    signOut: () => void,
-    deleteUser: () => void,
-    isLoading: boolean,
-    errorMessage: string | null
-    setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>
+  signInWithApple: () => void,
+  signInWithGoogle: () => void,
+  signInWithEmail: (data: SignInDto) => void,
+  signUpWithEmail: (data: SignUpDto) => void,
+  signOut: () => void,
+  deleteUser: () => void,
+  isLoading: boolean,
+  errorMessage: string | null
+  setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>
 }
 
 const AuthContext = React.createContext<AuthContextType>({
-    // user: null as AuthUser | null,
-    signInWithApple: () => {
-    },
-    signInWithGoogle: () => {
-    },
-    signInWithEmail: () => {
-    },
-    signUpWithEmail: () => {
-    },
-    signOut: () => {
-    },
-    deleteUser: () => {
+  // user: null as AuthUser | null,
+  signInWithApple: () => {
+  },
+  signInWithGoogle: () => {
+  },
+  signInWithEmail: () => {
+  },
+  signUpWithEmail: () => {
+  },
+  signOut: () => {
+  },
+  deleteUser: () => {
 
-    },
-    setErrorMessage: () => {
-    },
-    isLoading: false,
-    errorMessage: null,
+  },
+  setErrorMessage: () => {
+  },
+  isLoading: false,
+  errorMessage: null,
 });
 
 
-export const AuthProvider = ({children}: { children: React.ReactNode }) => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
-    // this is just for performance
-    useEffect(() => {
-        WebBrowser.warmUpAsync();
+  // this is just for performance
+  useEffect(() => {
+    WebBrowser.warmUpAsync();
 
-        return () => {
-            WebBrowser.coolDownAsync();
-        };
-    }, []);
-
-    const API = useApi();
-
-    const discovery: DiscoveryDocument = {
-        authorizationEndpoint: `${API.instance.getUri()}/api/auth/google/login`,
-        // tokenEndpoint: `${process.env.EXPO_PUBLIC_API_URL}/api/auth/google/token`,
-
+    return () => {
+      WebBrowser.coolDownAsync();
     };
-    const config: AuthRequestConfig = {
-        clientId: 'google',
-        redirectUri: makeRedirectUri(),
-        // scopes: ["openid", "profile", "email"], //defined in the backend
-    };
+  }, []);
 
-    const discoveryIOS: DiscoveryDocument = {
-        authorizationEndpoint: `${API.instance.getUri()}/api/auth/apple/login`,
+  const API = useApi();
 
-    };
-    const configIOS: AuthRequestConfig = {
-        clientId: 'apple',
-        redirectUri: makeRedirectUri(),
-    };
+  const discovery: DiscoveryDocument = {
+    authorizationEndpoint: `${API.instance.getUri()}/api/auth/google/login`,
+    // tokenEndpoint: `${process.env.EXPO_PUBLIC_API_URL}/api/auth/google/token`,
 
-    // const [user, setUser] = React.useState<AuthUser | null>(null);
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
-    const [request, response, promptAsync] = useAuthRequest(config, discovery);
-    const [requestIOS, responseIOS, promptAsyncIOS] = useAuthRequest(configIOS, discoveryIOS);
+  };
+  const config: AuthRequestConfig = {
+    clientId: 'google',
+    redirectUri: makeRedirectUri(),
+    // scopes: ["openid", "profile", "email"], //defined in the backend
+  };
 
-    // we are not using useAuthRequest because we are implementing oAuth2.0 with passport in the backend
-    // const [request, response, promptAsync] = useAuthRequest(config, discovery)
+  const discoveryIOS: DiscoveryDocument = {
+    authorizationEndpoint: `${API.instance.getUri()}/api/auth/apple/login`,
 
-    const {logIn, logOut} = useAuthStore();
+  };
+  const configIOS: AuthRequestConfig = {
+    clientId: 'apple',
+    redirectUri: makeRedirectUri(),
+  };
 
+  // const [user, setUser] = React.useState<AuthUser | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+  const [request, response, promptAsync] = useAuthRequest(config, discovery);
+  const [requestIOS, responseIOS, promptAsyncIOS] = useAuthRequest(configIOS, discoveryIOS);
 
-    WebBrowser.maybeCompleteAuthSession();
+  // we are not using useAuthRequest because we are implementing oAuth2.0 with passport in the backend
+  // const [request, response, promptAsync] = useAuthRequest(config, discovery)
 
-    const signInWithEmail = async (data: SignInDto) => {
-        try {
-            setIsLoading(true);
-            const res = await API.api.authControllerSignIn({email: data.email, password: data.password});
-            if (res.data) {
-                logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
-            }
-        } catch (err: any) {
-            setErrorMessage(err?.response?.data?.message);
-
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    const signUpWithEmail = async (signUpData: SignUpDto) => {
-
-        try {
-            setIsLoading(true);
-            const res = await API.api.authControllerSignUp(signUpData);
-            if (res.data) {
-                logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
-            }
-
-        } catch (err: any) {
-            setErrorMessage(err.response.data.message);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const { logIn, logOut } = useAuthStore();
 
 
-    const signInWithGoogle = async () => {
-        try {
-            if (!request) {
-                console.log('No request from google sign in');
-                return;
-            }
-            await promptAsync();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+  WebBrowser.maybeCompleteAuthSession();
 
-    const signInWithApple = async () => {
-        try {
-            if (!requestIOS) {
-                console.log('No request from Apple sign in');
-                return;
-            }
-            await promptAsyncIOS();
-        } catch (e) {
-            console.error(e);
-        }
-    };
+  const signInWithEmail = async (data: SignInDto) => {
+    try {
+      setIsLoading(true);
+      const res = await API.api.authControllerSignIn({ email: data.email, password: data.password });
+      if (res.data) {
+        logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
+      }
+    } catch (err: any) {
+      setErrorMessage(err?.response?.data?.message);
+
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const signUpWithEmail = async (signUpData: SignUpDto) => {
+
+    try {
+      setIsLoading(true);
+      const res = await API.api.authControllerSignUp(signUpData);
+      if (res.data) {
+        logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
+      }
+
+    } catch (err: any) {
+      setErrorMessage(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
 
-    const signOut = async () => {
-        try {
-            await API.api.authControllerSignOut();
-            logOut();
-        } catch (error) {
-            console.error(error);
-        }
+  const signInWithGoogle = async () => {
+    try {
+      if (!request) {
+        console.log('No request from google sign in');
+        return;
+      }
+      await promptAsync();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    };
+  const signInWithApple = async () => {
+    try {
+      if (!requestIOS) {
+        console.log('No request from Apple sign in');
+        return;
+      }
+      await promptAsyncIOS();
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    const deleteUser = async () => {
-        try {
-            await API.api.usersControllerDeleteUser()
-            logOut()
-        } catch (err) {
-            console.error(err)
-        }
+
+  const signOut = async () => {
+    try {
+      await API.api.authControllerSignOut();
+      logOut();
+    } catch (error) {
+      console.error(error);
     }
 
+  };
 
-    const exchangeWithToken = async (token: string) => {
-        if (!token) {
-            console.error('No token to exchange');
-            return
-        }
-        try {
-            const res = await API.api.authControllerExchangeToken({
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
-        } catch (err) {
-            console.error(err)
-        }
+  const deleteUser = async () => {
+    try {
+      await API.api.usersControllerDeleteUser();
+      logOut();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  const exchangeWithToken = async (token: string) => {
+    if (!token) {
+      console.error('No token to exchange');
+      return;
+    }
+    try {
+      const res = await API.api.authControllerExchangeToken({
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      logIn(res.data.accessToken, res.data.refreshToken, res.data.user.firstName, res.data.user.lastName, res.data.user.email);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  useEffect(() => {
+    const exchangeToken = async () => {
+      if (response?.type === 'success') {
+        await exchangeWithToken(response.params.exchangeToken);
+      }
     };
+    exchangeToken();
+  }, [response]);
 
+  useEffect(() => {
+    const exchangeToken = async () => {
+      if (responseIOS?.type === 'success') {
+        console.log(responseIOS);
+        await exchangeWithToken(responseIOS.params.exchangeToken);
+      }
+      if (responseIOS?.type === 'error') {
+        console.error(responseIOS?.errorCode);
+      }
+    };
+    exchangeToken();
+  }, [responseIOS]);
 
-    useEffect(() => {
-        const exchangeToken = async () => {
-            if (response?.type === 'success') {
-                await exchangeWithToken(response.params.exchangeToken);
-            }
-        };
-        exchangeToken();
-    }, [response]);
+  return (
+    <AuthContext.Provider value={{
+      signInWithApple,
+      signInWithGoogle,
+      signInWithEmail,
+      signUpWithEmail,
+      signOut,
+      deleteUser,
+      isLoading,
+      errorMessage,
+      setErrorMessage,
 
-    useEffect(() => {
-        const exchangeToken = async () => {
-            if (responseIOS?.type === 'success') {
-                await exchangeWithToken(responseIOS.params.exchangeToken);
-            }
-        };
-        exchangeToken();
-    }, [responseIOS]);
+    }}>
+      {children}
+    </AuthContext.Provider>
 
-    return (
-        <AuthContext.Provider value={{
-            signInWithApple,
-            signInWithGoogle,
-            signInWithEmail,
-            signUpWithEmail,
-            signOut,
-            deleteUser,
-            isLoading,
-            errorMessage,
-            setErrorMessage,
-
-        }}>
-            {children}
-        </AuthContext.Provider>
-
-    );
+  );
 };
 
 
 export const useAuth = () => {
-    const context = React.useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
 };
--- utils/authStore.ts
+++ utils/authStore.ts
@@ -1,86 +1,92 @@
-import {createJSONStorage, persist} from 'zustand/middleware';
-import {deleteItemAsync, getItem, setItem} from 'expo-secure-store';
-import {create} from 'zustand';
-import {UserType} from '@/types/user-type';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { deleteItemAsync, getItem, setItem } from 'expo-secure-store';
+import { create } from 'zustand';
+import { UserType } from '@/types/user-type';
 
 type UserInfo = {
-    firstName: string | null
-    lastName: string | null
-    email: string | null
+  firstName: string | null
+  lastName: string | null
+  email: string | null
 
 }
 
 type userState = {
-    isLoggedIn: boolean;
-    shouldCreateAccount: boolean;
-    hasCompletedOnboarding: boolean;
-    logIn: (accessToken: string, refreshToken: string, firstName: string, lastName: string, email: string) => void;
-    logOut: () => void;
-    completeOnboarding: () => void;
-    resetOnboarding: () => void;
-    userType: UserType
-    switchRole: (role: UserType) => void
-    user: UserInfo
+  isLoggedIn: boolean;
+  shouldCreateAccount: boolean;
+  hasCompletedOnboarding: boolean;
+  logIn: (accessToken: string, refreshToken: string, firstName?: string, lastName?: string, email?: string) => void;
+  logOut: () => void;
+  completeOnboarding: () => void;
+  resetOnboarding: () => void;
+  userType: UserType
+  switchRole: (role: UserType) => void
+  user: UserInfo
 }
 
 export const useAuthStore = create(persist<userState>((set) => ({
-    isLoggedIn: false,
-    shouldCreateAccount: false,
-    hasCompletedOnboarding: false,
-    userType: UserType.User,
-    user: {
-        firstName: null,
-        lastName: null,
-        email: null,
-    },
+  isLoggedIn: false,
+  shouldCreateAccount: false,
+  hasCompletedOnboarding: false,
+  userType: UserType.User,
+  user: {
+    firstName: null,
+    lastName: null,
+    email: null,
+  },
 
-    logIn: (accessToken: string, refreshToken: string, firstName: string, lastName: string, email: string) => set((state) => {
-        setItem('accessToken', accessToken);
-        setItem('refreshToken', refreshToken);
-        setItem('firstName', firstName);
-        setItem('lastName', lastName);
-        setItem('email', email);
-        return {
-            ...state,
-            isLoggedIn: true,
-            user: {
-                firstName, lastName, email,
-            },
-        };
-    }),
-    logOut: () => {
-        deleteItemAsync('accessToken');
-        deleteItemAsync('refreshToken');
-        deleteItemAsync('firstName');
-        deleteItemAsync('lastName');
-        deleteItemAsync('email');
-        set((state) => ({
-            ...state,
-            isLoggedIn: false,
-        }));
-    },
-    completeOnboarding: () => set((state) => {
-        return {
-            ...state,
-            hasCompletedOnboarding: true,
-        };
-    }),
-    resetOnboarding: () => set((state) => {
-        return {
-            ...state,
-            hasCompletedOnboarding: false,
-        };
-    }),
-    switchRole: (userType: UserType) => set((state) => {
-        return {
-            ...state,
-            userType: userType,
-        };
-    }),
+  logIn: (accessToken: string, refreshToken: string, firstName?: string, lastName?: string, email?: string) => set((state) => {
+    setItem('accessToken', accessToken);
+    setItem('refreshToken', refreshToken);
+    if (firstName) {
+      setItem('firstName', firstName);
+    }
+    if (lastName) {
+      setItem('lastName', lastName);
+    }
+    if (email) {
+      setItem('email', email);
+    }
+    return {
+      ...state,
+      isLoggedIn: true,
+      user: {
+        firstName: firstName ?? null, lastName: lastName ?? null, email: email ?? null,
+      },
+    };
+  }),
+  logOut: () => {
+    deleteItemAsync('accessToken');
+    deleteItemAsync('refreshToken');
+    deleteItemAsync('firstName');
+    deleteItemAsync('lastName');
+    deleteItemAsync('email');
+    set((state) => ({
+      ...state,
+      isLoggedIn: false,
+    }));
+  },
+  completeOnboarding: () => set((state) => {
+    return {
+      ...state,
+      hasCompletedOnboarding: true,
+    };
+  }),
+  resetOnboarding: () => set((state) => {
+    return {
+      ...state,
+      hasCompletedOnboarding: false,
+    };
+  }),
+  switchRole: (userType: UserType) => set((state) => {
+    return {
+      ...state,
+      userType: userType,
+    };
+  }),
 
 }), {
-    'name': 'auth-storage',
-    storage: createJSONStorage(() => ({
-        setItem, getItem, removeItem: deleteItemAsync,
-    })),
+  'name': 'auth-storage',
+  storage: createJSONStorage(() => ({
+    setItem, getItem, removeItem: deleteItemAsync,
+  })),
 }));

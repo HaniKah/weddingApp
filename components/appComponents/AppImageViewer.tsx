7acd21bd8f4efaeed6bcc127a275646d5b39d12f
hani <hanikahhaleh@gmail.com>
2026-05-12 02:32:41 +0200

    fix image ratio

--- components/appComponents/AppImageViewer.tsx
+++ components/appComponents/AppImageViewer.tsx
@@ -1,68 +1,68 @@
-import {Dimensions, Modal, Pressable, StyleSheet, View} from "react-native";
-import {useSafeAreaInsets} from "react-native-safe-area-context";
-import {IconSymbol} from "@/components/symbols/IconSymbol";
-import {PhotosDto} from "@/types/open-api";
-import ImageViewer from "react-native-image-zoom-viewer";
+import { Dimensions, Modal, Pressable, StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { IconSymbol } from '@/components/symbols/IconSymbol';
+import { PhotosDto } from '@/types/open-api';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
-export default function AppImageViewer({isVisible, onClose, images, activeIndex}: {
-    isVisible: boolean,
-    onClose: () => void,
-    images: PhotosDto[],
-    activeIndex: number
+export default function AppImageViewer({ isVisible, onClose, images, activeIndex }: {
+  isVisible: boolean,
+  onClose: () => void,
+  images: PhotosDto[],
+  activeIndex: number
 
 }) {
-    const {width} = Dimensions.get('window')
-    const insets = useSafeAreaInsets();
+  const { width } = Dimensions.get('window');
+  const insets = useSafeAreaInsets();
 
-    function ImageHeader() {
-        return (
-            <>
-                <View style={styles.headerContainer}>
-                    <Pressable onPress={onClose}>
-                        <IconSymbol color="white" size={25} name="xmark"/>
-                    </Pressable>
-                </View>
-            </>
-        )
-    }
-
-    const ViewableImages = images.map((i) => {
-        return {
-            url: i.uri,
-            width: width,
-            height: width * i.ratio
-        }
-    })
+  function ImageHeader() {
     return (
-        <>
-            <Modal
-                allowSwipeDismissal={true}
-                visible={isVisible}
-                onRequestClose={onClose}
-                animationType="none">
-                <View style={{paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1, backgroundColor: "black"}}>
-                    <ImageViewer
-                        pageAnimateTime={200}
-                        renderHeader={ImageHeader}
-                        onSwipeDown={() => onClose()}
-                        index={activeIndex} enableSwipeDown={true}
-                        imageUrls={ViewableImages}>
+      <>
+        <View style={styles.headerContainer}>
+          <Pressable onPress={onClose}>
+            <IconSymbol color="white" size={25} name="xmark" />
+          </Pressable>
+        </View>
+      </>
+    );
+  }
 
-                    </ImageViewer>
-                </View>
+  const ViewableImages = images.map((i) => {
+    return {
+      url: i.uri,
+      width: width,
+      height: width / i.ratio,
+    };
+  });
+  return (
+    <>
+      <Modal
+        allowSwipeDismissal={true}
+        visible={isVisible}
+        onRequestClose={onClose}
+        animationType="none">
+        <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom, flex: 1, backgroundColor: 'black' }}>
+          <ImageViewer
+            pageAnimateTime={200}
+            renderHeader={ImageHeader}
+            onSwipeDown={() => onClose()}
+            index={activeIndex} enableSwipeDown={true}
+            imageUrls={ViewableImages}>
 
-            </Modal>
-        </>
-    )
+          </ImageViewer>
+        </View>
+
+      </Modal>
+    </>
+  );
 }
 const styles = StyleSheet.create({
 
-    headerContainer: {
-        display: "flex",
-        flexDirection: "row-reverse",
-        paddingTop: 20,
-        paddingHorizontal: 20,
-    }
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
 
 
-})
+});

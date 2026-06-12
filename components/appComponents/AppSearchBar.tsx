9a0d81d167b90af0bbd8c9cc41132cd54139c63a
hani <hanikahhaleh@gmail.com>
2026-06-13 00:43:30 +0300

    changes on the app search bar

--- components/appComponents/AppSearchBar.tsx
+++ components/appComponents/AppSearchBar.tsx
@@ -1,5 +1,6 @@
-import AppTextInput from './AppTextInput';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
+import {Theme} from "@/styles/Theme";
+import {IconButton} from "@/components/symbols/IconButton";
 
 export default function AppSearchBar({searchText, setSearchText}: {
     searchText: string | undefined,
@@ -9,12 +10,18 @@ export default function AppSearchBar({searchText, setSearchText}: {
     return (
         <>
             <View style={styles.container}>
-                <AppTextInput design={2}
-                              placeholder="search"
-                              value={searchText}
-                              name="search"
-                              onChange={(text) => setSearchText(text)}
-                />
+                {/*<AppTextInput design={2}*/}
+                {/*              placeholder="search"*/}
+                {/*              value={searchText}*/}
+                {/*              name="search"*/}
+                {/*              onChange={(text) => setSearchText(text)}*/}
+                {/*              extraStyles={styles.input}*/}
+                {/*/>*/}
+                <TextInput placeholder="Search"
+                           value={searchText}
+                           onChangeText={setSearchText}
+                           style={styles.input}/>
+                <IconButton extraStylesBtn={styles.filterButton} size={24} name="line.3.horizontal.decrease"/>
             </View>
         </>
     );
@@ -23,5 +30,22 @@ const styles = StyleSheet.create({
     container: {
         marginBottom: 15,
         marginTop: 5,
+        borderBottomWidth: 0,
+        boxShadow: Theme.effects.boxShadow,
+        borderRadius: Theme.radius.xl,
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingRight: 5,
+        backgroundColor: Theme.colors.white,
     },
+    input: {
+        borderBottomWidth: 0,
+        flex: 1,
+        paddingLeft: 20,
+
+    },
+    filterButton: {
+        backgroundColor: Theme.colors.background
+    }
 });

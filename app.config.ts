e2089fccb32305fdd83d45418bcbc488d68bf294
hani <hanikahhaleh@gmail.com>
2026-06-23 20:24:34 +0300

    change version number

--- app.config.ts
+++ app.config.ts
@@ -69,7 +69,7 @@ export default ({config}: ConfigContext): ExpoConfig => {
         ...config,
         'name': name,
         'slug': 'weddingApp',
-        'version': '1.0.1',
+        'version': '1.1.0',
         'orientation': 'portrait',
         'icon': icon,
         'scheme': scheme,

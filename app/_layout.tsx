8f84a6574c520bb8b65764462c94e597bf3de096
hani <hanikahhaleh@gmail.com>
2026-06-24 14:30:39 +0300

    delete the complete-oauth.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -84,10 +84,9 @@ export default function RootLayout() {
                 <GestureHandlerRootView>
                     <LocationProvider>
                         <Stack screenOptions={{headerShown: false}}>
-                            {/*<Stack.Screen name="complete-oauth"/>*/}
+                            <Stack.Screen name="(tabs)"/>
                             <Stack.Screen name="pick-location"/>
                             <Stack.Screen name="+not-found"/>
-                            <Stack.Screen name="(tabs)"/>
                         </Stack>
                     </LocationProvider>
                     <StatusBar style="dark"/>

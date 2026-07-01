6d48526ffff175bb346aa9bda5bbc9b4081365f2
hani <hanikahhaleh@gmail.com>
2026-07-01 22:09:19 +0300

    fix late addDressesFeature

--- components/wizards/createPlaceWizard/AddFeaturesDress.tsx
+++ components/wizards/createPlaceWizard/AddFeaturesDress.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from "react-native";
 import {useTranslation} from "react-i18next";
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 import {UpdateStep, VendorPlaceDetailsDto} from "@/types/open-api";
 import RadioButtonElement from "@/components/RadioButtonElement";
 import {Theme} from "@/styles/Theme";
@@ -8,43 +8,50 @@ import WizardController from "@/components/wizards/WizardController";
 import {useApi} from "@/utils/api";
 import {useWizardContext} from "@/components/wizards/Wizard";
 import FeaturesHero from "@/components/wizards/createPlaceWizard/FeaturesHero";
+import {showSnackbar} from "@/components/Snackbar";
+import AppView from "@/components/appComponents/AppView";
 
 export default function AddFeaturesDress({data, setData}: {
     data: VendorPlaceDetailsDto,
     setData: Dispatch<SetStateAction<VendorPlaceDetailsDto | undefined>>
 }) {
     const [rent, setRent] = useState<boolean | undefined>(data.features?.rent)
+    const [isLoading, setIsLoading] = useState(false)
     const {api} = useApi()
     const wizard = useWizardContext();
     const {t} = useTranslation()
 
 
-    useEffect(() => {
-        async function updateFeatures() {
-            try {
-                const res = await api.placesControllerUpdatePlace({
-                    updateStep: UpdateStep.AddFeatures,
-                    id: data.id,
+    async function updateFeatures() {
+        try {
+            setIsLoading(true)
+            const res = await api.placesControllerUpdatePlace({
+                updateStep: UpdateStep.AddFeatures,
+                id: data.id,
+                features: {
+                    category: data.category,
                     features: {
-                        category: data.category,
-                        features: {
-                            rent: rent,
-                        }
+                        rent: rent,
                     }
-                })
-                setData(res.data)
-            } catch (err) {
-                console.error(err)
-            }
+                }
+            })
+            setData(res.data)
+        } catch {
+            showSnackbar("unknown error", "error")
+        } finally {
+            setIsLoading(false)
         }
+    }
 
-        updateFeatures()
-    }, [rent])
+    async function handleNext() {
+        await updateFeatures()
+        wizard.nextStep()
+    }
 
 
     return (
         <>
-            <View style={styles.container}>
+            <AppView isLoading={isLoading} extraStyles={styles.container}>
                 <FeaturesHero category={data?.category}/>
                 <Text style={styles.question}>{t("Do you offer dress rentals ?")}</Text>
                 <Text
@@ -60,12 +67,12 @@ export default function AddFeaturesDress({data, setData}: {
 
                     <RadioButtonElement
                         icon="xmark"
-                        value={data?.features?.rent}
+                        value={rent}
                         item={{value: false, name: "No, not yet"}}
                         onChange={setRent}/>
                 </View>
-            </View>
-            <WizardController onNext={wizard.nextStep}/>
+            </AppView>
+            <WizardController onNext={handleNext}/>
         </>
     )
 }

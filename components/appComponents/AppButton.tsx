1ca2de93975a8d72869276a78f0e722a57feb162
hani <hanikahhaleh@gmail.com>
2026-07-04 15:31:01 +0300

    fix PLAIN button

--- components/appComponents/AppButton.tsx
+++ components/appComponents/AppButton.tsx
@@ -1,203 +1,203 @@
-import { StyleProp, Text, TextStyle, ViewStyle } from 'react-native';
-import { ButtonSize, ButtonStyles, ButtonType } from '@/styles/Button';
-import { useFormContext } from '@/contexts/form-context';
-import { IconSymbol } from '@/components/symbols/IconSymbol';
-import { SFSymbols6_0 } from 'sf-symbols-typescript';
-import { Theme } from '@/styles/Theme';
-import { SvgProps } from 'react-native-svg';
-import { FC } from 'react';
+import {StyleProp, Text, TextStyle, ViewStyle} from 'react-native';
+import {ButtonSize, ButtonStyles, ButtonType} from '@/styles/Button';
+import {useFormContext} from '@/contexts/form-context';
+import {IconSymbol} from '@/components/symbols/IconSymbol';
+import {SFSymbols6_0} from 'sf-symbols-typescript';
+import {Theme} from '@/styles/Theme';
+import {SvgProps} from 'react-native-svg';
+import {FC} from 'react';
 import Animated from 'react-native-reanimated';
 import AppPressable from '@/components/appComponents/AppPressable';
 
 export default function AppButton({
-                                    buttonType,
-                                    buttonSize,
-                                    isSubmit,
-                                    onPress,
-                                    children,
-                                    extraStylesBtn,
-                                    extraStylesTxt,
-                                    icon,
-                                    iconColor,
-                                    CustomIcon,
-                                    destructive,
-                                    confirmative,
-                                    informative,
-                                    fullWidth,
-                                    underline,
-                                    inactive,
-                                    textPosition,
-                                    fullRound,
+                                      buttonType,
+                                      buttonSize,
+                                      isSubmit,
+                                      onPress,
+                                      children,
+                                      extraStylesBtn,
+                                      extraStylesTxt,
+                                      icon,
+                                      iconColor,
+                                      CustomIcon,
+                                      destructive,
+                                      confirmative,
+                                      informative,
+                                      fullWidth,
+                                      underline,
+                                      inactive,
+                                      textPosition,
+                                      fullRound,
 
 
                                   }: {
-  buttonSize?: 'SM' | 'MD' | 'LG',
-  buttonType?: ButtonType,
-  isSubmit?: boolean,
-  onPress?: () => void,
-  children?: React.ReactNode | string,
-  extraStylesBtn?: StyleProp<ViewStyle>,
-  extraStylesTxt?: StyleProp<TextStyle>,
-  icon?: SFSymbols6_0,
-  iconColor?: string,
-  CustomIcon?: FC<SvgProps>,
-  destructive?: boolean,
-  confirmative?: boolean,
-  informative?: boolean,
-  fullWidth?: boolean,
-  underline?: boolean,
-  inactive?: boolean,
-  textPosition?: 'LEFT' | 'RIGHT'
-  fullRound?: boolean,
+    buttonSize?: 'SM' | 'MD' | 'LG',
+    buttonType?: ButtonType,
+    isSubmit?: boolean,
+    onPress?: () => void,
+    children?: React.ReactNode | string,
+    extraStylesBtn?: StyleProp<ViewStyle>,
+    extraStylesTxt?: StyleProp<TextStyle>,
+    icon?: SFSymbols6_0,
+    iconColor?: string,
+    CustomIcon?: FC<SvgProps>,
+    destructive?: boolean,
+    confirmative?: boolean,
+    informative?: boolean,
+    fullWidth?: boolean,
+    underline?: boolean,
+    inactive?: boolean,
+    textPosition?: 'LEFT' | 'RIGHT'
+    fullRound?: boolean,
 
 
 }) {
-  let buttonStyles: StyleProp<ViewStyle> = {};
-  let textStyles: StyleProp<TextStyle> = {};
-  let iconSize: number;
+    let buttonStyles: StyleProp<ViewStyle> = {};
+    let textStyles: StyleProp<TextStyle> = {};
+    let iconSize: number;
 
 
-  if (buttonType === ButtonType.OUTLINED) {
-    buttonStyles = ButtonStyles.outlinedBtn;
-    textStyles = ButtonStyles.outlinedTxt;
-    if (destructive) {
-      buttonStyles = { ...buttonStyles, borderColor: Theme.colors.red.S500 };
-      textStyles = { ...textStyles, color: Theme.colors.red.S500 };
-    }
-    if (confirmative) {
-      buttonStyles = { ...buttonStyles, borderColor: Theme.colors.green.S600 };
-      textStyles = { ...textStyles, color: Theme.colors.green.S600 };
-    }
-    if (informative) {
-      buttonStyles = { ...buttonStyles, borderColor: Theme.colors.blue.S500 };
-      textStyles = { ...textStyles, color: Theme.colors.blue.S500 };
-    }
-    if (inactive) {
-      buttonStyles = { ...buttonStyles, borderColor: Theme.colors.gray.S300 };
-      textStyles = { ...textStyles, color: Theme.colors.gray.S500 };
-    }
+    if (buttonType === ButtonType.OUTLINED) {
+        buttonStyles = ButtonStyles.outlinedBtn;
+        textStyles = ButtonStyles.outlinedTxt;
+        if (destructive) {
+            buttonStyles = {...buttonStyles, borderColor: Theme.colors.red.S500};
+            textStyles = {...textStyles, color: Theme.colors.red.S500};
+        }
+        if (confirmative) {
+            buttonStyles = {...buttonStyles, borderColor: Theme.colors.green.S600};
+            textStyles = {...textStyles, color: Theme.colors.green.S600};
+        }
+        if (informative) {
+            buttonStyles = {...buttonStyles, borderColor: Theme.colors.blue.S500};
+            textStyles = {...textStyles, color: Theme.colors.blue.S500};
+        }
+        if (inactive) {
+            buttonStyles = {...buttonStyles, borderColor: Theme.colors.gray.S300};
+            textStyles = {...textStyles, color: Theme.colors.gray.S500};
+        }
 
-  } else if (buttonType === ButtonType.PLAIN) {
-    buttonStyles = ButtonStyles.plainBtn;
-    textStyles = ButtonStyles.plainTxt;
-    if (destructive) {
-      textStyles = { ...textStyles, color: Theme.colors.red.S500 };
-    }
-    if (confirmative) {
-      textStyles = { ...textStyles, color: Theme.colors.green.S600 };
-    }
-    if (informative) {
-      textStyles = { ...textStyles, color: Theme.colors.blue.S500 };
-    }
-    if (inactive) {
-      textStyles = { ...textStyles, color: Theme.colors.gray.S500 };
-    }
-  } else {
-    buttonStyles = ButtonStyles.primaryBtn;
-    textStyles = ButtonStyles.primaryTxt;
-    if (destructive) {
-      buttonStyles = { ...buttonStyles, backgroundColor: Theme.colors.red.S100 };
-      textStyles = { ...textStyles, color: Theme.colors.red.S500 };
-    }
-    if (confirmative) {
-      buttonStyles = { ...buttonStyles, backgroundColor: Theme.colors.green.S100 };
-      textStyles = { ...textStyles, color: Theme.colors.green.S600 };
-    }
-    if (informative) {
-      buttonStyles = { ...buttonStyles, backgroundColor: Theme.colors.blue.S100 };
-      textStyles = { ...textStyles, color: Theme.colors.blue.S500 };
-    }
-    if (inactive) {
-      buttonStyles = { ...buttonStyles, backgroundColor: Theme.colors.gray.S300 };
-      textStyles = { ...textStyles, color: Theme.colors.gray.S600 };
-    }
-  }
-
-  switch (buttonSize) {
-    case ButtonSize.SM:
-      buttonStyles = { ...buttonStyles, ...ButtonStyles.smSizeBtn };
-      textStyles = { ...textStyles, ...ButtonStyles.smSizeTxt };
-      iconSize = Theme.sizes.md;
-      break;
-    case ButtonSize.LG:
-      buttonStyles = { ...buttonStyles, ...ButtonStyles.lgSizeBtn };
-      textStyles = { ...textStyles, ...ButtonStyles.lgSizeTxt };
-      iconSize = Theme.sizes.xxl;
-      break;
-    default:
-      buttonStyles = { ...buttonStyles, ...ButtonStyles.mdSizeBtn };
-      textStyles = { ...textStyles, ...ButtonStyles.mdSizeTxt };
-      iconSize = Theme.sizes.xl;
-      break;
-  }
-
-  if (buttonType === ButtonType.PLAIN) {
-    buttonStyles.paddingHorizontal = 0;
-    buttonStyles.paddingVertical = 0;
-  }
-
-
-  if (underline) {
-    textStyles.textDecorationLine = 'underline';
-  }
-  if (fullWidth) {
-    buttonStyles.alignSelf = 'stretch';
-  }
-  if (fullRound) {
-    buttonStyles.borderRadius = Theme.radius.full;
-  }
-
-  switch (textPosition) {
-    case 'LEFT':
-      buttonStyles.justifyContent = 'flex-start';
-      break;
-    case 'RIGHT':
-      buttonStyles.justifyContent = 'flex-end';
-      break;
-  }
-
-  const formContext = useFormContext();
-
-
-  const handleOnPress = () => {
-
-    if (isSubmit) {
-      formContext.setSubmitting(false); // Reset to false first to ensure transition
-      setTimeout(() => {
-        formContext.setSubmitting(true);
-      }, 0);
-      return;
+    } else if (buttonType === ButtonType.PLAIN) {
+        buttonStyles = ButtonStyles.plainBtn;
+        textStyles = ButtonStyles.plainTxt;
+        if (destructive) {
+            textStyles = {...textStyles, color: Theme.colors.red.S500};
+        }
+        if (confirmative) {
+            textStyles = {...textStyles, color: Theme.colors.green.S600};
+        }
+        if (informative) {
+            textStyles = {...textStyles, color: Theme.colors.blue.S500};
+        }
+        if (inactive) {
+            textStyles = {...textStyles, color: Theme.colors.gray.S500};
+        }
     } else {
-      if (onPress) {
-        onPress();
-      }
-      return;
+        buttonStyles = ButtonStyles.primaryBtn;
+        textStyles = ButtonStyles.primaryTxt;
+        if (destructive) {
+            buttonStyles = {...buttonStyles, backgroundColor: Theme.colors.red.S100};
+            textStyles = {...textStyles, color: Theme.colors.red.S500};
+        }
+        if (confirmative) {
+            buttonStyles = {...buttonStyles, backgroundColor: Theme.colors.green.S100};
+            textStyles = {...textStyles, color: Theme.colors.green.S600};
+        }
+        if (informative) {
+            buttonStyles = {...buttonStyles, backgroundColor: Theme.colors.blue.S100};
+            textStyles = {...textStyles, color: Theme.colors.blue.S500};
+        }
+        if (inactive) {
+            buttonStyles = {...buttonStyles, backgroundColor: Theme.colors.gray.S300};
+            textStyles = {...textStyles, color: Theme.colors.gray.S600};
+        }
     }
-  };
+
+    switch (buttonSize) {
+        case ButtonSize.SM:
+            buttonStyles = {...buttonStyles, ...ButtonStyles.smSizeBtn};
+            textStyles = {...textStyles, ...ButtonStyles.smSizeTxt};
+            iconSize = Theme.sizes.md;
+            break;
+        case ButtonSize.LG:
+            buttonStyles = {...buttonStyles, ...ButtonStyles.lgSizeBtn};
+            textStyles = {...textStyles, ...ButtonStyles.lgSizeTxt};
+            iconSize = Theme.sizes.xxl;
+            break;
+        default:
+            buttonStyles = {...buttonStyles, ...ButtonStyles.mdSizeBtn};
+            textStyles = {...textStyles, ...ButtonStyles.mdSizeTxt};
+            iconSize = Theme.sizes.xl;
+            break;
+    }
+
+    // if (buttonType === ButtonType.PLAIN) {
+    //     buttonStyles.paddingHorizontal = 6;
+    //     buttonStyles.paddingVertical = 4;
+    // }
 
 
-  return (
-    <>
+    if (underline) {
+        textStyles.textDecorationLine = 'underline';
+    }
+    if (fullWidth) {
+        buttonStyles.alignSelf = 'stretch';
+    }
+    if (fullRound) {
+        buttonStyles.borderRadius = Theme.radius.full;
+    }
 
-      <AppPressable disabled={inactive}
-                    onPress={handleOnPress}
+    switch (textPosition) {
+        case 'LEFT':
+            buttonStyles.justifyContent = 'flex-start';
+            break;
+        case 'RIGHT':
+            buttonStyles.justifyContent = 'flex-end';
+            break;
+    }
 
-      >
-        <Animated.View style={[buttonStyles, extraStylesBtn]}>
-          {CustomIcon && <CustomIcon />}
+    const formContext = useFormContext();
 
-          {icon &&
-            <IconSymbol size={iconSize} color={iconColor || textStyles.color || 'black'} name={icon} />
-          }
-          {
-            children && <Text style={[textStyles, extraStylesTxt]}>
-              {children}
-            </Text>
-          }
 
-        </Animated.View>
-      </AppPressable>
-    </>
+    const handleOnPress = () => {
 
-  );
+        if (isSubmit) {
+            formContext.setSubmitting(false); // Reset to false first to ensure transition
+            setTimeout(() => {
+                formContext.setSubmitting(true);
+            }, 0);
+            return;
+        } else {
+            if (onPress) {
+                onPress();
+            }
+            return;
+        }
+    };
+
+
+    return (
+        <>
+
+            <AppPressable disabled={inactive}
+                          onPress={handleOnPress}
+
+            >
+                <Animated.View style={[buttonStyles, extraStylesBtn]}>
+                    {CustomIcon && <CustomIcon/>}
+
+                    {icon &&
+                        <IconSymbol size={iconSize} color={iconColor || textStyles.color || 'black'} name={icon}/>
+                    }
+                    {
+                        children && <Text style={[textStyles, extraStylesTxt]}>
+                            {children}
+                        </Text>
+                    }
+
+                </Animated.View>
+            </AppPressable>
+        </>
+
+    );
 }

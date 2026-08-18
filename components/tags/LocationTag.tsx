21e673a6af4b7d0b86d181fb1f2c406499ac1be1
hani <hanikahhaleh@gmail.com>
2026-08-18 15:52:43 +0300

    translate location tag

--- components/tags/LocationTag.tsx
+++ components/tags/LocationTag.tsx
@@ -1,45 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Theme } from '@/styles/Theme';
-import { IconSymbol } from '@/components/symbols/IconSymbol';
-import { CountryCode } from '@/types/open-api';
-import { COUNTRIES } from '@/constants/countries';
+import {StyleSheet, Text, View} from 'react-native';
+import {Theme} from '@/styles/Theme';
+import {IconSymbol} from '@/components/symbols/IconSymbol';
+import {CountryCode} from '@/types/open-api';
+import {COUNTRIES} from '@/constants/countries';
+import {useTranslation} from "react-i18next";
 
-export default function LocationTag({ countryCode, city, removeBackground, textColor }: {
-  countryCode?: CountryCode,
-  city?: string,
-  removeBackground?: boolean
-  textColor?: string
+export default function LocationTag({countryCode, city, removeBackground, textColor}: {
+    countryCode?: CountryCode,
+    city?: string,
+    removeBackground?: boolean
+    textColor?: string
 }) {
-  if (countryCode || city) return (
-    <View style={[styles.container, removeBackground && styles.removeBackground]}>
-      <IconSymbol name="location" size={14} color={textColor ?? Theme.colors.primary} />
-      <Text style={[styles.text, textColor && { color: textColor }]}>
-        {countryCode ? COUNTRIES.get(countryCode)?.countryName : city}
-      </Text>
-    </View>
+    const {t} = useTranslation();
+    if (countryCode || city) return (
+        <View style={[styles.container, removeBackground && styles.removeBackground]}>
+            <IconSymbol name="location" size={14} color={textColor ?? Theme.colors.primary}/>
+            <Text style={[styles.text, textColor && {color: textColor}]}>{
+                countryCode ? t(`countries.${COUNTRIES.get(countryCode)?.countryName}`) : t(`cities.${city}`)
+            }
 
-  );
+            </Text>
+        </View>
+
+    );
 }
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Theme.colors.iconBackground,
-    alignSelf: 'flex-start',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 5,
-    paddingHorizontal: 12,
-    paddingVertical: 5,
-    borderRadius: Theme.radius.full,
+    container: {
+        backgroundColor: Theme.colors.iconBackground,
+        alignSelf: 'flex-start',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 5,
+        paddingHorizontal: 12,
+        paddingVertical: 5,
+        borderRadius: Theme.radius.full,
 
-  },
-  text: {
-    fontSize: Theme.sizes.xs,
-    color: Theme.colors.primary,
-  },
-  removeBackground: {
-    paddingVertical: 0,
-    paddingHorizontal: 0,
-    backgroundColor: 'transparent',
-  },
+    },
+    text: {
+        fontSize: Theme.sizes.xs,
+        color: Theme.colors.primary,
+    },
+    removeBackground: {
+        paddingVertical: 0,
+        paddingHorizontal: 0,
+        backgroundColor: 'transparent',
+    },
 });
